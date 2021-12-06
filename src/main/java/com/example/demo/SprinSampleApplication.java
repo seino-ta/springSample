59dca59c17f32b96a79bba2867f96db5dd733df1
seino_ta <seino_ta@works-hi.co.jp>
2021-12-06 13:25:27 +0900

    first commit

---
+++ src/main/java/com/example/demo/SprinSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinSampleApplication.class, args);
+	}
+
+}
